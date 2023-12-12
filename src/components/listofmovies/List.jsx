e1c544ef6afcadc9677e989f0d09f06b7ec97617
YashRajput233 <yashp7813@gmail.com>
2023-12-12 18:20:01 +0530

    new updates

--- src/components/listofmovies/List.jsx
+++ src/components/listofmovies/List.jsx
@@ -1,18 +1,35 @@
+import React, { useState } from "react";
+import Poppage from "../Popuppage/Poppage";
 import "./List.css";
 import { listofMovies1, listofMovies2, listofMovies3 } from "../../data/data";
-import {useState} from 'react';
-import Poppage from "../Popuppage/Poppage";
 
 function List() {
   const [isOpen, setIsOpen] = useState(false);
+  const [selectedMovieId, setSelectedMovieId] = useState(null);
+const [selectedList, setSelectedList] = useState("listofMovies1");
+  const handleMovieClick = (movieId,list) => {
+    setSelectedMovieId(movieId);
+    setSelectedList(list)
+    setIsOpen(true);
+  };
+
+  const closePoppage = () => {
+    setIsOpen(false);
+    setSelectedMovieId(null);
+  };
   return (
     <>
       <div className="list_section">
         <h2>Hindi Movie & TV</h2>
         <div className="main_movie_div">
-          {listofMovies1.map((user) => (
-            <div>
-              <img className="movie_img" onClick={() => setIsOpen(true)} src={user.imageName} />
+          {listofMovies1.map((user, i) => (
+            <div key={i}>
+              <img
+                className="movie_img"
+                onClick={() => handleMovieClick(user.id,"listofMovies1")}
+                src={user.imageName}
+                alt={`Movie ${user.id}`}
+              />
             </div>
           ))}
         </div>
@@ -20,9 +37,14 @@ function List() {
       <div className="list_section">
         <h2>Trending Now</h2>
         <div className="main_movie_div">
-          {listofMovies2.map((user) => (
-            <div>
-              <img className="movie_img" onClick={() => setIsOpen(true)} src={user.imageName} />
+          {listofMovies2.map((user, i) => (
+            <div key={i}>
+              <img
+                className="movie_img"
+                onClick={() => handleMovieClick(user.id,"listofMovies2")}
+                src={user.imageName}
+                alt={`Movie ${user.id}`}
+              />
             </div>
           ))}
         </div>
@@ -30,20 +52,27 @@ function List() {
       <div className="list_section">
         <h2>Only on Netflix</h2>
         <div className="main_movie_div">
-          {listofMovies3.map((user) => (
-            <div>
-              <img className="movie_img" onClick={() => setIsOpen(true)} src={user.imageName} />
+          {listofMovies3.map((user, i) => (
+            <div key={i}>
+              <img
+                className="movie_img"
+                onClick={() => handleMovieClick(user.id,"listofMovies3")}
+                src={user.imageName}
+                alt={`Movie ${user.id}`}
+              />
             </div>
           ))}
         </div>
       </div>
-      {isOpen && (
-       <>
-        <Poppage closebtn={() => setIsOpen(false)}/>
-       </>
+      {isOpen && selectedMovieId !== null && (
+        <Poppage
+          movieId={selectedMovieId}
+          closebtn={closePoppage}
+          listofMovies1={listofMovies1} listofMovies2={listofMovies2} listofMovies3={listofMovies3}
+          selectedList={selectedList}
+        />
       )}
     </>
   );
 }
-
 export default List;
--- src/router/Routers.jsx
+++ src/router/Routers.jsx
@@ -1,19 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Navigate, Link } from "react-router-dom";
 import Home from '../pages/Home';
-import Poppage from '../components/Popuppage/Poppage';
+import View from '../components/View/View';
 const Routers = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="add/:id" element={<Poppage />} />
-      {/* <Route path="/404" element={<NotFoundPage />} />
-      <Route path="/apple">
-        <Route path="/" element={<ApplePage />} />
-        <Route path="*" element={<Navigate replace to="/apple" />} />
-      </Route>
-      <Route path="*" element={<Navigate replace to="/404" />} /> */}
+      <Route path="/View" element={<View />} />
     </Routes>
   </BrowserRouter>
   )
